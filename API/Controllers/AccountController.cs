ff275ccff2ad2d8cf332070967cabc41847a8e87
Michiel van 't Klooster <mvtklooster@hotmail.com>
2022-11-29 15:05:56 +0100

    End of section 4

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -3,6 +3,8 @@ using System.Text;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,15 +14,16 @@ public class AccountController : BaseApiController
 {
 
     private readonly DataContext _context;
+    private readonly ITokenService _tokenService;
 
-    public AccountController(DataContext context)
+    public AccountController(DataContext context, ITokenService tokenService)
     {
         _context = context;
-
+        _tokenService = tokenService;
     }
 
     [HttpPost("register")]
-    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
     {
         if (await UserExists(registerDto.Username)) return BadRequest("Username is taken.");
 
@@ -35,7 +38,34 @@ public class AccountController : BaseApiController
 
         _context.Add(user);
         await _context.SaveChangesAsync();
-        return user;
+        return new UserDto
+        {
+            Username = user.UserName,
+            Token = _tokenService.CreateToken(user)
+        };
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+    {
+        AppUser user = await _context.Users.FirstOrDefaultAsync(x => 
+            x.UserName == loginDto.Username.ToLower());
+
+        if (user == null) return Unauthorized("Invalid username.");
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+        Byte[] computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+        for (int i = 0; i < user.PasswordHash.Length; i++)
+        {
+            if (user.PasswordHash[i] != computedHash[i]) return Unauthorized("Invalid password.");
+        }
+
+        return new UserDto
+        {
+            Username = user.UserName,
+            Token = _tokenService.CreateToken(user)
+        };
     }
 
     private async Task<bool> UserExists(string username)
